9bf4863505501ae4bfabf6190757bc7323d0b758
JennaSigman <123jello@gmail.com>
2024-10-14 23:37:56 -0400

    3.7 not rendering

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,158 +1,37 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink, useParams} from "react-router-dom";
 import "../style.css";
+
 export default function CoursesNavigation() {
+  const { courseId } = useParams(); // Retrieve course ID from URL params
+
+  const links = [
+    "Home",
+    "Modules",
+    "Piazza",
+    "Zoom",
+    "Assignments",
+    "Quizzes",
+    "Grades",
+    "People"
+  ];
+
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       <nav>
-        <NavLink
-          to="/Kanbas/Courses/1234/Home"
-          id="wd-course-home-link"
-          className={({ isActive }) =>
-            isActive
-              ? "list-group-item custom-active border border-0"
-              : "list-group-item custom-inactive border border-0"
-          }
-        >
-          {" "}
-          Home{" "}
-        </NavLink>
-
-        <NavLink
-          to="/Kanbas/Courses/1234/Modules"
-          id="wd-course-home-link"
-          className={({ isActive }) =>
-            isActive
-              ? "list-group-item custom-active border border-0"
-              : "list-group-item custom-inactive border border-0"
-          }
-        >
-          {" "}
-          Modules{" "}
-        </NavLink>
-
-        <NavLink
-          to="/Kanbas/Courses/1234/Piazza"
-          id="wd-course-home-link"
-          className={({ isActive }) =>
-            isActive
-              ? "list-group-item custom-active border border-0"
-              : "list-group-item custom-inactive border border-0"
-          }
-        >
-          {" "}
-          Piazza{" "}
-        </NavLink>
-
-        <NavLink
-          to="/Kanbas/Courses/1234/Zoom"
-          id="wd-course-home-link"
-          className={({ isActive }) =>
-            isActive
-              ? "list-group-item custom-active border border-0"
-              : "list-group-item custom-inactive border border-0"
-          }
-        >
-          {" "}
-          Zoom{" "}
-        </NavLink>
-
-        <NavLink
-          to="/Kanbas/Courses/1234/Assignments"
-          id="wd-course-home-link"
-          className={({ isActive }) =>
-            isActive
-              ? "list-group-item custom-active border border-0"
-              : "list-group-item custom-inactive border border-0"
-          }
-        >
-          {" "}
-          Assignments{" "}
-        </NavLink>
-
-        <NavLink
-          to="/Kanbas/Courses/1234/Quizzes"
-          id="wd-course-home-link"
-          className={({ isActive }) =>
-            isActive
-              ? "list-group-item custom-active border border-0"
-              : "list-group-item custom-inactive border border-0"
-          }
-        >
-          {" "}
-          Quizzes{" "}
-        </NavLink>
-
-        <NavLink
-          to="/Kanbas/Courses/1234/People"
-          id="wd-course-home-link"
-          className={({ isActive }) =>
-            isActive
-              ? "list-group-item custom-active border border-0"
-              : "list-group-item custom-inactive border border-0"
-          }
-        >
-          {" "}
-          People{" "}
-        </NavLink>
+        {links.map((link) => (
+          <NavLink
+            key={link}
+            to={`/Kanbas/Courses/${courseId}/${link}`}
+            className={({ isActive }) =>
+              isActive
+                ? "list-group-item custom-active border border-0"
+                : "list-group-item custom-inactive border border-0"
+            }
+          >
+            {link}
+          </NavLink>
+        ))}
       </nav>
-
-      {/* <Link
-        to="/Kanbas/Courses/1234/Home"
-        id="wd-course-home-link"
-        className="list-group-item active border border-0"
-      >
-        {" "}
-        Home{" "}
-      </Link>
-
-      <Link
-        to="/Kanbas/Courses/1234/Modules"
-        id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Modules{" "}
-      </Link>
-      <Link
-        to="/Kanbas/Courses/1234/Piazza"
-        id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Piazza{" "}
-      </Link>
-      <Link
-        to="/Kanbas/Courses/1234/Zoom"
-        id="wd-course-zoom-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Zoom{" "}
-      </Link>
-      <Link
-        to="/Kanbas/Courses/1234/Assignments"
-        id="wd-course-quizzes-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Assignments{" "}
-      </Link>
-      <Link
-        to="/Kanbas/Courses/1234/Quizzes"
-        id="wd-course-assignments-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        Quizzes{" "}
-      </Link>
-      <Link
-        to="/Kanbas/Courses/1234/People"
-        id="wd-course-people-link"
-        className="list-group-item text-danger border border-0"
-      >
-        {" "}
-        People{" "}
-      </Link> */}
     </div>
   );
 }
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -3,16 +3,20 @@ import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
-import { Route, Routes } from "react-router";
+import { Route, Routes, useParams, useLocation } from "react-router";
 import { FaAlignJustify } from "react-icons/fa";
 import PeopleTable from "./People/Table";
+import { courses } from "../Database";
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
         <FaAlignJustify className="me-4 fs-4 mb-1" />
-        Course 1234{" "}
-      </h2>{" "}
+        {course && course.name} &gt; {pathname.split("/")[4]}
+      </h2>
       <hr />
       <div className="d-flex">
         <div className="d-none d-md-block">
@@ -24,7 +28,7 @@ export default function Courses() {
             <Route path="Modules" element={<Modules />} />
             <Route path="Assignments" element={<Assignments />} />
             <Route path="Editor" element={<AssignmentEditor />} />
-            <Route path="People" element={<PeopleTable />} /> 
+            <Route path="People" element={<PeopleTable />} />
           </Routes>
         </div>
       </div>
