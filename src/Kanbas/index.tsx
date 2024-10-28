8eedbdcbee01265ca9b528d0770733e0d53ad156
JennaSigman <123jello@gmail.com>
2024-10-28 00:19:45 -0400

    completed 3.2

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -5,7 +5,60 @@ import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./style.css";
+import * as db from "./Database";
+import { useState } from "react";
+
 export default function Kanbas() {
+   
+  const [courses, setCourses] = useState<any[]>(db.courses);
+  const [course, setCourse] = useState<any>({
+    _id: "0",
+    name: "New Course",
+    number: "New Number",
+    startDate: "2023-09-10",
+    endDate: "2023-12-15",
+    image: "/images/reactjs.jpg",
+    description: "New Description",
+  });
+
+  const addNewCourse = () => {
+    const newCourse = { ...course, _id: new Date().getTime().toString() };
+    setCourses([...courses, newCourse]);
+    resetCourseForm();
+  };
+
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((c) => c._id !== courseId));
+  };
+
+  const editCourse = (selectedCourse: any) => {
+    setCourse(selectedCourse);
+  };
+
+  const resetCourseForm = () => {
+    setCourse({
+      _id: "0",
+      name: "New Course",
+      number: "New Number",
+      startDate: "2023-09-10",
+      endDate: "2023-12-15",
+      image: "/images/reactjs.jpg",
+      description: "New Description",
+    });
+  };
+
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
   return (
     <div id="wd-kanbas">
       <KanbasNavigation />
@@ -13,8 +66,17 @@ export default function Kanbas() {
         <Routes>
           <Route path="/" element={<Navigate to="Account" />} />
           <Route path="/Account/*" element={<Account />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/Courses/:cid/*" element={<Courses />} />
+          <Route path="/Dashboard" element={
+            <Dashboard               
+              courses={courses}
+              course={course}
+              setCourse={setCourse}
+              addNewCourse={addNewCourse}
+              deleteCourse={deleteCourse}
+              updateCourse={updateCourse}
+              editCourse={editCourse}/>
+            } />
+          <Route path="/Courses/:cid/*" element={<Courses courses={courses} />} />
           <Route path="/Calendar*" element={<h2>Calendar </h2>} />
           <Route path="/Inbox*" element={<h2>Inbox</h2>} />
         </Routes>
