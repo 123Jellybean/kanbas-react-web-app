edd233e9054ce8445c25086cc6ba983eb19b8819
JennaSigman <123jello@gmail.com>
2024-12-04 00:14:02 -0500

    up to 3.6.7

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -37,6 +37,31 @@ export const signout = async () => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
   return response.data;
 };
+
+export const findAllUsers = async () => {
+  const response = await axiosWithCredentials.get(USERS_API);
+  return response.data;
+};
+
+export function findUsersByRole(role: string) {
+  throw new Error("Function not implemented.");
+}
+
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
+
+export const findUserById = async (id: string) => {
+  const response = await axios.get(`${USERS_API}/${id}`);
+  return response.data;
+};
+
+export const deleteUser = async (userId: string) => {
+  const response = await axios.delete(`${USERS_API}/${userId}`);
+  return response.data;
+};
+
 export const updateUser = async (user: any) => {
   const response = await axiosWithCredentials.put(
     `${USERS_API}/${user._id}`,
@@ -44,9 +69,3 @@ export const updateUser = async (user: any) => {
   );
   return response.data;
 };
-
-export const findAllUsers = async () => {
-  const response = await axiosWithCredentials.get(USERS_API);
-  return response.data;
-};
-
